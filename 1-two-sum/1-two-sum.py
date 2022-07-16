20cbec13de8d9ca7298721bec3ca11b8298a1730
AmmarAlani <60627873+AmmarAlani@users.noreply.github.com>
2022-07-15 22:23:47 -0600

    Time: 58 ms (98.11%), Space: 15.2 MB (50.46%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hash = {}
+        for i,n in enumerate(nums):
+            val = target - n
+            if val in hash:
+                return [hash[val],i]
+            else:
+                hash [n] = i
+        
